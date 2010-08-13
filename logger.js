decdb437f78fe6f0c3895d21af27e80ca68e2b55
Huseyin Yilmaz <yilmazhuseyin@gmail.com>
2010-08-13 10:27:42 +0300

    Added timing Problem with ie was fixed 	modified:   logger.js 	modified:   sample.html

--- logger.js
+++ logger.js
@@ -1,21 +1,40 @@
-var logger = {
-	enableLog:false,
-	loggedFunctionArray: [],
-	log: function(message){
-		if (this.enableLog === true && window.console != undefined) {
-			var prefix = "";
-			for (var i = 1; i < this.loggedFunctionArray.length; i++) 
-				prefix += "\t";
-			console.log(prefix + this.loggedFunctionArray[this.loggedFunctionArray.length - 1] + " : ", message);
-		}
-	},//log
-	startLog: function(functionName){
-		this.loggedFunctionArray.push(functionName);
-		this.log("Start");
-	},//startLog
-	endLog: function(){
-		//var loggedFunction = this.loggedFunctionArray[this.loggedFunctionArray.length-1];
-		this.log("End");
-		this.loggedFunctionArray.pop();
-	},//startLog
-};//logger
+var logger = function(){
+		var loggedFunctionArray = [];
+	return {
+		enableLog:false,
+		log: function(message){
+			if (this.enableLog === true && window.console != undefined) {
+				var prefix = "";
+				for (var i = 1; i < loggedFunctionArray.length; i++) 
+					prefix += "\t";
+				console.log(prefix + loggedFunctionArray[loggedFunctionArray.length - 1] + " : ", message);
+			}
+		},//log
+		warn: function(message){
+			if (this.enableLog === true && window.console != undefined) {
+				var prefix = "";
+				for (var i = 1; i < loggedFunctionArray.length; i++) 
+					prefix += "\t";
+				console.warn(prefix + loggedFunctionArray[loggedFunctionArray.length - 1] + " : ", message);
+			}
+		},//log
+	
+		startLog: function(functionName){
+			if (this.enableLog === true && window.console != undefined) {
+				loggedFunctionArray.push(functionName);
+				this.log("Start");
+				if(console.time)
+					console.time(functionName);
+			};
+		},//startLog
+		endLog: function(){
+			if (this.enableLog === true && window.console != undefined) {
+				this.log("End");
+				if(console.time)
+					console.timeEnd(loggedFunctionArray.pop());
+				else
+					loggedFunctionArray.pop();
+			};
+		}//endLog
+	};//logger
+}();
